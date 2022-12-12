98637ac4c4b80398f267fc24c9cff9da6093a7ef
Nour Knefed <nourknefed@gmail.com>
2022-12-12 11:15:23 +0100

    init

---
+++ calculator.js
@@ -0,0 +1,85 @@
+const currentOperation_text = document.getElementById('current-operation');
+const perviousOperation_text = document.getElementById('pervious-operation');
+
+export class Calculator {
+  constructor(previous_operation, current_operation) {
+    this.previous_operation = previous_operation;
+    this.current_operation = current_operation;
+    this.clear();
+  }
+
+  clear() {
+    this.current_operation = "";
+    this.previous_operation = "";
+    this.operation = undefined;
+    this.error = "";
+  }
+
+  delete() {
+    this.current_operation = this.current_operation.slice(0, -1);
+    display.value = this.current_operation;
+  }
+
+  displayNumber(number) {
+    if (number === '.' && this.current_operation.includes('.')) return
+    this.current_operation = this.current_operation.toString() + number.toString();
+  }
+
+  chooseOperation(operation) {
+    if (this.current_operation === "") return
+    if (this.previous_operation !== "") {
+      this.operate()
+    }
+
+    this.operation = operation;
+    this.previous_operation = this.current_operation;
+    this.current_operation = "";
+  }
+
+  operate() {
+    let result;
+    const previous = parseFloat(this.previous_operation)
+    const current = parseFloat(this.current_operation)
+    if (isNaN(previous) || isNaN(current)) return;
+    switch (this.operation) {
+      case '+':
+        result = previous + current
+        break;
+      case '-':
+        result = previous - current
+        break;
+      case '*':
+        result = previous * current
+        break;
+      case '÷':
+        if (current === 0) {
+          this.error = "Can't divide by zero"
+        }
+        result = previous / current
+        break;
+      default: return
+    }
+    //this.current_operation = result;
+    this.current_operation = +(Math.round(result + "e+3") + "e-3");
+    this.operation = undefined;
+    this.previous_operation = "";
+  }
+
+  updateResult() {
+    if (this.error) {
+      currentOperation_text.innerText = this.error;
+      return
+    }
+    currentOperation_text.innerText = this.current_operation;
+    if (this.operation != null) {
+      perviousOperation_text.innerText = `${this.previous_operation} ${this.operation}`
+    }
+    else {
+      perviousOperation_text.innerText = "";
+    }
+  }
+}
+
+
+
+
---
+++ script.js
@@ -0,0 +1,41 @@
+import { Calculator } from './calculator.js';
+
+const numberBtns = document.querySelectorAll('[data-number]');
+const operationBtns = document.querySelectorAll('[data-operation]');
+const equalsBtn = document.getElementById('equals');
+const clearBtn = document.getElementById('clear');
+const deleteBtn = document.getElementById('del');
+const perviousOperation_text = document.getElementById('pervious-operation');
+const currentOperation_text = document.getElementById('current-operation');
+
+const calculator = new Calculator(perviousOperation_text, currentOperation_text)
+
+
+numberBtns.forEach(btn => {
+  btn.addEventListener('click', () => {
+    calculator.displayNumber(btn.innerHTML);
+    calculator.updateResult();
+  })
+})
+
+for (const operataion of operationBtns) {
+  operataion.addEventListener('click', () => {
+    calculator.chooseOperation(operataion.innerHTML);
+    calculator.updateResult();
+  })
+}
+
+equalsBtn.addEventListener('click', () => {
+  calculator.operate();
+  calculator.updateResult();
+})
+
+clearBtn.addEventListener('click', () => {
+  calculator.clear();
+  calculator.updateResult();
+});
+
+deleteBtn.addEventListener('click', () => {
+  calculator.delete();
+  calculator.updateResult();
+});
